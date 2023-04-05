357ec579f37208b6596890ecf700d8d29d8faf5b
xuanthaidk <74334975+xuanthaidk@users.noreply.github.com>
2023-04-05 23:08:34 +0700

    add cong, tru, nhan, chia, tinh khoang cach voi class Point3D

---
+++ Point3D/Point3D/Point3D.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point
+{
+    public class Point3D
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+
+        public Point3D(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static Point3D operator +(Point3D p1, Point3D p2)
+        {
+            return new Point3D(p1.x + p2.x, p1.y + p2.y, p1.z + p2.z);
+        }
+
+        public static Point3D operator -(Point3D p1, Point3D p2)
+        {
+            return new Point3D(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+        }
+
+        public static Point3D operator *(Point3D p1, float n)
+        {
+            return new Point3D(p1.x * n, p1.y * n, p1.z * n);
+        }
+
+        public static Point3D operator /(Point3D p1, float n)
+        {
+            return new Point3D(p1.x / n, p1.y / n, p1.z / n);
+        }
+
+        public static double d(Point3D p1, Point3D p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.x - p2.x, 2) + Math.Pow(p1.y - p2.y, 2) + Math.Pow(p1.z - p2.z, 2));
+        }
+
+        public override string? ToString()
+        {
+            return "(" + x + ", " + y + ", " + z + ")";
+        }
+
+        public static Point3D InputPoint()
+        {
+            Console.WriteLine("Nhap toa do x y z:");
+            float x = Convert.ToInt32(Console.ReadLine());
+            float y = Convert.ToInt32(Console.ReadLine());
+            float z = Convert.ToInt32(Console.ReadLine());
+            return new Point3D(x, y, z);
+        }
+
+    }
+}
---
+++ Point3D/Point3D/Program.cs
@@ -0,0 +1,61 @@
+﻿using Point;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+class Program
+{
+    public static Point3D p1;   
+    public static Point3D p2;   
+    private static void Main(string[] args)
+    {
+        Calculate();
+    }
+
+    private static void Calculate()
+    {
+        bool input = true;
+        while (input)
+        {
+            Console.WriteLine("1. Cong");
+            Console.WriteLine("2. Trư");
+            Console.WriteLine("3. Nhan");
+            Console.WriteLine("4. Chia");
+            Console.WriteLine("5. Khoang cach");
+            Console.WriteLine("6. Thoat");
+
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    p1 = Point3D.InputPoint();
+                    p2 = Point3D.InputPoint();
+                    Console.WriteLine("P1 + P2 = " + (p1 + p2).ToString());
+                    break;
+                case 2:
+                    p1 = Point3D.InputPoint();
+                    p2 = Point3D.InputPoint();
+                    Console.WriteLine("P1 - P2 = " + (p1 - p2).ToString());
+                    break;
+                case 3:
+                    p1 = Point3D.InputPoint();
+                    Console.WriteLine("Nhap he so: ");
+                    Console.WriteLine("P1 * P2 = " + (p1 * Convert.ToInt32(Console.ReadLine())));
+                    break;
+                case 4:
+                    p1 = Point3D.InputPoint();
+                    Console.WriteLine("Nhap he so: ");
+                    Console.WriteLine("P1 * P2 = " + (p1 / Convert.ToInt32(Console.ReadLine())));
+                    break;
+                case 5:
+                    p1 = Point3D.InputPoint();
+                    p2 = Point3D.InputPoint();
+                    Console.WriteLine("Khoang cach 2 điem: " + Point3D.d(p1, p2));
+                    break;
+                case 6:
+                    input = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
